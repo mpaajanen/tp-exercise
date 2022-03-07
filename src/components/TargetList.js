c1b546eb98f5386999cf28f8561daf015997bf1a
mikkopaa <mikko.paajanen@metropolia.fi>
2022-03-08 01:37:56 +0200

    Korjattu kohdelistan kaatuminen, kun haku kesken

--- src/components/TargetList.js
+++ src/components/TargetList.js
@@ -1,12 +1,23 @@
-import React from "react"
-import { useSelector } from "react-redux"
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 const TargetList = () => {
   const targets = useSelector(state => state.targets)
-  console.log(targets)
-
+  // console.log(targets)
+  if (targets.length===0) {
+    return (
+      <div>Ladataan...</div>
+    )
+  }
   return (
     <div>
+      {targets.giftTargets.map(target =>
+        <div key={target.giftTargetId}>
+          <div>
+            {target.name}
+          </div>
+        </div>
+      )}
     </div>
   )
 
