7ab38d90f67ee364473a58efffdce531364c39c1
mikkopaa <mikko.paajanen@metropolia.fi>
2022-03-07 15:49:06 +0200

    react-router-dom käyttöön

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 export default function App() {
+  const axios = require('axios').default
+
+  axios.get('https://test.tpfons.fi/tpdemo/ext/back/api/v1-0000/donation/targets')
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+  
   return (
     <div>
       <h1>Otsikko</h1>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Page1 from './routes/Page1'
+import Page2 from './routes/Page2'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <App /> } />
+      <Route path='/page1' element={ <Page1 /> } />
+      <Route path='/page2' element={ <Page2 /> } />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 )
