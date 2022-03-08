1edc4976d45d461cdd59595c810255b87ee9339d
mikkopaa <mikko.paajanen@metropolia.fi>
2022-03-08 13:58:08 +0200

    Tuotantodatan url lisätty ja kokeiltu

--- src/services/targets.js
+++ src/services/targets.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const baseUrl = 'https://test.tpfons.fi/tpdemo/ext/back/api/v1-0000/donation/targets'
+// const baseUrl = 'https://tpfons.fi/tpdemo/ext/back/api/v1-0000/donation/targets'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
