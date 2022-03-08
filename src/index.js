0c0d14d19a0bd09fac412430d4014e9e4ab21baf
mikkopaa <mikko.paajanen@metropolia.fi>
2022-03-08 13:51:41 +0200

    Kohteiden routtaus ja paluu listaan

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Page1 from './routes/Page1'
-import Page2 from './routes/Page2'
-import Targets from './routes/Targets'
+// import Page1 from './routes/Page1'
+// import Page2 from './routes/Page2'
+// import Targets from './routes/Targets'
 import Target from './routes/Target'
 import { store } from './store'
 
@@ -14,13 +14,9 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={ <App /> }>
-          <Route path='page1' element={ <Page1 /> } />
-          <Route path='page2' element={ <Page2 /> } />
-          <Route path='targets' element={ <Targets /> } >
-            <Route path=':targetId' element={ <Target /> } />
-          </Route>
           <Route path='*' element={ <div>Sivua ei löytynyt.</div> } />
         </Route>
+        <Route path='targets/:targetId' element={ <Target /> } />
       </Routes>
     </BrowserRouter>
   </Provider>,
--- src/routes/Target.js
+++ src/routes/Target.js
@@ -1,9 +1,14 @@
 import { useParams } from "react-router-dom"
+import { Link } from 'react-router-dom'
+
 
 const Target = () => {
   let params = useParams()
   return (
-    <h2>Kohde: {params.targetId}</h2>
+    <div>
+      <Link to={'/'}>Takaisin</Link>
+      <h2>Kohde: {params.targetId}</h2>
+    </div>
   )
 }
 
